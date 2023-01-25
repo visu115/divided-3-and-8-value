4cd31aedc1cce0957d5b3b13048e6a5a4fd4a78c
visu <visuvisu115@gmai.com>
2023-01-25 09:17:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// if you have the value is above 100 to 500  and you set division 3 and divison 8 
+// and only print the division answer
+
+
+for(i=100;i<500;i++)
+{
+if(i % 3==0 && i%8==0){
+
+console.log(i);
+}
+}
